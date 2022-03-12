2abda96dd43a7fd25873bdceb25418e136e8cca4
Frandey Briceño <f.briceno@correo.unimet.edu.ve>
2022-03-12 14:08:04 -0400

    Le puse Homepage pero es más Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,26 @@ import Homepage from "./pages/Homepage"
 import Loginpage from "./pages/Loginpage"
 import Register from "./pages/Register"
 import Error404 from "./pages/Error404"
-import Navbar from './components/NavBar/Navbar.module';
-import { Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/NavBar/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
 
 
     <Router>
+    
+    <Navbar />
     <div className="App">
     <h1>Peliculitas</h1>
     </div>
-    <Navbar/>
     
       <Routes>
-      <Route path="/" element ={<Homepage/>}/>
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Loginpage />} />
+      <Route exact path="/" element ={"..."}/>
+      <Route exact path="/register" element={"..."} />
+      <Route exact path="/login" element={"..."} />
       <Route path="*" element={<Error404 />} />
       </Routes>
-    
-
-
-
     </Router>
   );
 }
