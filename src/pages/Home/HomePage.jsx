b0e03ed55aa982e90c2dd3ec71a643004ed16f77
Frandey Briceño <f.briceno@correo.unimet.edu.ve>
2022-03-13 16:22:47 -0400

    Revert "Firebase"
    This reverts commit 9a91275a537ef2a3d76a75cc48da21e6eb112cb0.

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,58 +1,13 @@
 
-import {useEffect, useState} from "react"
-import {db} from "../../utils/firebase-config"
+// import {useEffect, useState} from "react"
+// import {db} from "../../utils/firebase-config"
 import MovieFront from "../../components/MovieFront/MovieFront"
 
 
 export default function Homepage(){
-const [movies, setMovies] = useState([]);
-
-const addNewMovie = (newMovie)=> {
-    setMovies([...movies, newMovie]);
-
-}
-
-const fetchPelis = () => {
-    const pelis = db.collection("peliculas")
-    pelis
-    .get()
-    .then((data)=>{
-        const arr = [];
-
-        data.docs.forEach((element)=>{
-            console.log({data: element.data()})
-            arr.push({...element.data})
-
-        });
-
-        setMovies(arr);
-
-
-    })
-    .catch((error) => {
-        console.log(error);
-
-    });
-
-}
-
-useEffect(() =>{
-    fetchPelis();
-    
-}, [])
-
-
     return(
         <div>
-            
             <MovieFront/>
-            
-
-            
-        
-                
-        
-
         </div>
     )
 
